768e52baee418a9d306ca6781bd3cdd4f5ccc76f
testjokerqwerty@gmail.com <testjokerqwerty@gmail.com>
2021-07-18 14:19:08 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Search />
+  </div>
+</template>
+
+<script>
+import Search from "./components/search.vue";
+export default {
+  name: "App",
+  components: { Search },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
